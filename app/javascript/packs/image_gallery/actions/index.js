35e829c84ce4d1d5ddd5ed9e64c46e892d6eb858
Jeff Knox <knoxjeffrey@outlook.com>
2017-09-19 03:08:40 +0100

    uploader working with progress indicator

--- app/javascript/packs/image_gallery/actions/index.js
+++ app/javascript/packs/image_gallery/actions/index.js
@@ -1,4 +1,6 @@
 import request from 'superagent';
+require('es6-promise/auto');
+import browserImageSize from 'browser-image-size'
 let _csrf_param = () => { return document.getElementsByName("csrf-param")[0].content }
 let _csrf_token = () => { return document.getElementsByName("csrf-token")[0].content }
 
@@ -30,31 +32,33 @@ export const removeSelectedImage = selectedImageId => {
 }
 
 export const createImage = payload => {
+  const random = Math.random().toString(36).substring(7);
+  // const size = {width: 270, height: 180}
   return function(dispatch, getState) {
-    const random = Math.random().toString(36).substring(7);
-    dispatch(addUpload({
-      id: random,
-      upload: payload.upload,
-      preview: payload.preview,
-      progress: 0,
-      state: "started",
-      previewImgSize: payload.previewImgSize
-    }))
-    payload.formData.append(_csrf_param(), _csrf_token())
-    request.post('/admin/images')
-      .send(payload.formData).set('Accept', 'application/json')
-      .on('progress', function(e){
-        dispatch(updateUploadProgress({id: random, progress: e.percent}))
+    browserImageSize(payload.file.preview)
+      .then(function (size) {
+        return size
       })
-      .end(function(err,resp){
-        if(err) {
-          console.error(err)
-        } else {
-          dispatch({
-            type: 'ADD_TO_LOADED_IMAGES',
-            payload: resp.body
+      .then(function (size) {
+        dispatch(addUpload(uploadArrayObject(payload, random, size)))
+      })
+      .then(function () {
+        payload.formData.append(_csrf_param(), _csrf_token())
+        request.post('/admin/images')
+          .send(payload.formData).set('Accept', 'application/json')
+          .on('progress', function(e){
+            dispatch(updateUploadProgress({id: random, uploadProgressValue: e.percent, state: getState()}))
           })
-        }
+          .end(function(err,resp){
+            if(err) {
+              dispatch(updateUploadState({id: random, state: "error"}))
+            } else {
+              dispatch({
+                type: 'ADD_TO_LOADED_IMAGES',
+                payload: {body: resp.body, id: random}
+              })
+            }
+          })
       })
   }
 }
@@ -66,9 +70,63 @@ export function addUpload(upload) {
   }
 }
 
-export function updateUploadProgress(upload) {
+export function updateUploadProgress(data) {
+  var payload = uploadProgress(data)
   return {
     type: "UPDATE_UPLOAD_PROGRESS",
-    payload: upload
+    payload: payload
+  }
+}
+
+export function updateUploadState(upload) {
+  var payload = uploadProgress(data)
+  return {
+    type: "UPDATE_UPLOAD_STATE",
+    payload: payload
+  }
+}
+
+// helper functions
+
+export function uploadArrayObject(payload, random, size) {
+ return {
+   id: random,
+   state: "started",
+   upload: payload.upload,
+   build_for_gallery : buildForGallery(payload, size),
+   data: {"alt_text": ""},
+   uploadProgressValue: 0
+ }
+}
+
+export function buildForGallery(payload, size) {
+  return {
+    id: Math.floor(Math.random() * 1000000000),
+    src: payload.file.preview,
+    thumbnail: payload.file.preview,
+    caption: "",
+    thumbnailWidth: size.width,
+    thumbnailHeight: size.height,
+    isSelected: false,
+    editPath: ""
+  }
+}
+
+export function uploadProgress(data) {
+  var state = data.state.loadedImages
+  var img = state[state.findIndex(x => x.id == data.id)];
+  var newArray = state.slice();
+  newArray.splice(newArray.findIndex(x => x.id == data.id), 1);
+
+  if(img !== undefined) {
+    if(data.uploadProgressValue !== undefined) {
+      var newImg = Object.assign({},img, { uploadProgressValue: data.uploadProgressValue })
+    } else {
+      var newImg = Object.assign({},img)
+    }
+    newArray.unshift(newImg)
+    return newArray;
+  } else {
+    return state;
   }
 }
